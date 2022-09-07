3113fa529a13ff670f6800f0efc1fb2d7671d081
i304804 <frantisek.kolar@sap.com>
2022-09-07 10:52:57 +0200

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {MobileModeConfig} from "@fundamental-ngx/core";
+import {MultiComboboxSelectionChangeEvent} from "@fundamental-ngx/platform";
+import {HttpClient} from "@angular/common/http";
+import {User} from "./domain/ model/user.mode";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
-  title = 'multicombo-events';
+export class AppComponent implements OnInit {
+  dataSource = [
+    {name: 'Apple', type: 'Fruits'},
+    {name: 'Banana', type: 'Fruits'},
+    {name: 'Pineapple', type: 'Fruits'},
+    {name: 'Strawberry', type: 'Fruits'},
+    {name: 'Broccoli', type: 'Vegetables'},
+    {name: 'Carrot', type: 'Vegetables'},
+    {name: 'Jalapeño', type: 'Vegetables'},
+    {name: 'Spinach', type: 'Vegetables'}
+  ];
+
+  data: User[] = [];
+
+  selectedItems = [];
+
+  mobileConfig: MobileModeConfig = {
+    title: 'Title',
+    approveButtonText: 'Save',
+    cancelButtonText: 'Cancel',
+    hasCloseButton: true
+  };
+
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+
+  ngOnInit() {
+    this.httpClient.get<User[]>("assets/data.json").subscribe(data => {
+      this.data = data;
+    })
+  }
+
+  onSelect(item: MultiComboboxSelectionChangeEvent): void {
+    this.selectedItems = item.selectedItems;
+
+    console.log('onSelect called', item);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,54 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {FdpFormGroupModule, PlatformComboboxModule, PlatformMultiComboboxModule} from '@fundamental-ngx/platform/form';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  BusyIndicatorModule,
+  ContentDensityModule,
+  DialogConfig,
+  MOBILE_MODE_CONFIG,
+  MobileModeConfigToken,
+  MobileModeControl
+} from "@fundamental-ngx/core";
+import {HttpClientModule} from "@angular/common/http";
+
+
+export const MOBILE_DIALOG_PORTRAIT: DialogConfig = {
+  width: '360px',
+  height: '640px'
+};
+
+export const MOBILE_DIALOG_LANDSCAPE: DialogConfig = {
+  width: '640px',
+  height: '360px'
+};
+
+
+export const MULTI_COMBOBOX_MOBILE_CONFIG: MobileModeConfigToken = {
+  target: MobileModeControl.MULTI_COMBOBOX,
+  config: { dialogConfig: MOBILE_DIALOG_PORTRAIT }
+};
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FdpFormGroupModule,
+    PlatformComboboxModule,
+    PlatformMultiComboboxModule,
+    BusyIndicatorModule,
+    BrowserAnimationsModule,
+    ContentDensityModule.forRoot({ storage: 'localStorage' })
   ],
-  providers: [],
+  providers: [
+    { provide: MOBILE_MODE_CONFIG, useValue: MULTI_COMBOBOX_MOBILE_CONFIG, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
