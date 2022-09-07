caa955358ec70a9a502fe078e11a0f61cc83a8f3
i304804 <frantisek.kolar@sap.com>
2022-09-07 12:02:05 +0200

    Remove absolete code

--- src/app/domain/ds/users.ds.ts
+++ src/app/domain/ds/users.ds.ts
@@ -12,40 +12,4 @@ export class UserDataSource extends MultiComboBoxDataSource<User> {
   constructor(dataProvider: UserDataProvider) {
     super(dataProvider)
   }
-
-
-  override match(predicate?: string | Map<string, string>) {
-    this._onDataRequested$.next();
-    this._dataLoading = true;
-    const searchParam = new Map();
-
-    if (typeof predicate === 'string') {
-      searchParam.set('query', predicate);
-    } else if (predicate instanceof Map) {
-      predicate.forEach((v, k) => searchParam.set(k, v));
-    } else {
-      throw new Error('DataSource.match() predicate can only accepts string and Map');
-    }
-
-    if (!searchParam.has('limit') && !this.limitless) {
-      searchParam.set('limit', ComboBoxDataSource.MaxLimit);
-    }
-
-    this.dataProvider
-      .fetch(searchParam)
-      .pipe(
-        takeUntil(this._onDestroy$),
-      )
-      .subscribe(
-        (result: User[]) => {
-          this._onDataReceived$.next();
-          this._dataLoading = false;
-          this._dataChanges.next(result);
-        },
-        () => {
-          this._onDataReceived$.next();
-          this._dataLoading = false;
-        }
-      );
-  }
 }
